9329cbe55395cc6cbb8a47d568fd4bc25bcea32c
Naoto Asai <himeringo4854@gamil.com>
2020-05-12 22:16:07 +0900

    ローカルストレージAPIの追加

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+// https://jp.vuejs.org/v2/examples/todomvc.html
+var STORAGE_KEY = 'todos-vuejs-demo'
+var todoStorage = {
+  fetch: function() {
+    var todos = JSON.parse(
+      localStorage.getItem(STORAGE_KEY) || '[]'
+    )
+    todos.forEach(function(todo, index) {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
+
+
 const app = new Vue({
   el: '#app',
   data: {
